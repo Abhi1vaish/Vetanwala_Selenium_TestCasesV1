0f03893c4d0897700d2214303ff392e9681264aa
Abhi1Vaish <abhishekvaish962gmail.com>
2026-02-19 17:22:33 +0530

    Employee login latest change

--- src/test/java/org/example/Employeelogin2.java
+++ src/test/java/org/example/Employeelogin2.java
@@ -1,5 +1,6 @@
 package org.example;
 
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,22 +11,14 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import java.time.Duration;
 
-public class Employeelogin2 {
+public class Employeelogin2 extends BaseTest {
 
-    WebDriver driver;
-    WebDriverWait wait;
+    SoftAssert softAssert = new SoftAssert();
 
-    @BeforeMethod
-    public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-
-        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-    }
 
     @Test
     public void EmpLogin() {
@@ -37,21 +30,13 @@ public class Employeelogin2 {
         driver.findElement(By.id("password")).sendKeys("12345678");
         driver.findElement(By.id("submit-login")).click();
 
-        // Wait until URL changes (login success)
-        wait.until(ExpectedConditions.urlContains("dashboard"));
-
         String currentUrl = driver.getCurrentUrl();
         System.out.println("After Login URL: " + currentUrl);
 
-        Assert.assertTrue(currentUrl.contains("dashboard"), "Login failed - Dashboard not loaded");
+        //Assert.assertTrue(currentUrl.contains("dashboard"), "Login failed - Dashboard not loaded");
+        softAssert.assertEquals(currentUrl.contains("dashboard"), "Login failed - Dashboard not loaded");
 
         System.out.println("✔ Login successful");
     }
 
-    @AfterMethod
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
 }
