79dc69d1d7b30372f11a4f9626414fc01f5c8c14
Abhi1Vaish <abhishekvaish962gmail.com>
2026-03-06 18:18:34 +0530

    My Profile on dashboard verification
    (cherry picked from commit e07c4b2b726a6fc61094c1b85f039bddb7e7d6c6)

---
+++ src/test/java/org/example/dashboard/Dashboardprofile.java
@@ -0,0 +1,42 @@
+package org.example.dashboard;
+
+import org.example.BaseTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Dashboardprofile extends BaseTest {
+@BeforeMethod
+    public void setup(){
+
+        driver.get("https://vetanwala.com/login");
+        driver.findElement(By.id("email")).sendKeys("abhishekvaish@thesileo.com");
+        driver.findElement(By.id("password")).sendKeys("12345678");
+        driver.findElement(By.id("submit-login")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlContains("dashboard"));
+
+        String currentUrl = driver.getCurrentUrl();
+        softAssert.assertTrue(currentUrl.contains("dashboard"), "Dashboard not open after login");
+
+        System.out.println("Dashboard Open Successfully");
+    }
+
+    @Test
+    public void DashboardMyProfile(){
+
+        WebElement myProfileLink = driver.findElement(By.xpath("//*[@id=\"fullscreen\"]/div[4]/button[2]"));
+        myProfileLink.click();
+        String variable = myProfileLink.getText();
+        softAssert.assertTrue(variable.contains("Profile"),"My profile title is present not on Dashboard page");
+        System.out.println("Profile Page Open Successfully");
+
+    }
+
+}
